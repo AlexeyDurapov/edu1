d31511b8c2ddee353a6688c67deaf981e8113301
Alexey Durapov <alex-sgb@mail.ru>
2020-12-08 02:41:19 +0300

    MySQL damp file updated

--- dump_testextjs.sql
+++ dump_testextjs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost:3306
--- Время создания: Дек 07 2020 г., 01:52
+-- Время создания: Дек 08 2020 г., 02:38
 -- Версия сервера: 5.7.32-0ubuntu0.18.04.1
 -- Версия PHP: 7.2.24-0ubuntu0.18.04.7
 
@@ -19,6 +19,43 @@ SET time_zone = "+00:00";
 --
 -- База данных: `testextjs`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `cities`
+--
+
+CREATE TABLE `cities` (
+  `id` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `cities`
+--
+
+INSERT INTO `cities` (`id`, `name`) VALUES
+(1, 'Москва'),
+(2, 'Санкт-Петербург'),
+(3, 'Новосибирск'),
+(4, 'Екатеринбург'),
+(5, 'Казань'),
+(6, 'Нижний Новгород'),
+(7, 'Челябинск'),
+(8, 'Самара'),
+(9, 'Омск'),
+(10, 'Ростов-на-Дону'),
+(11, 'Уфа'),
+(12, 'Красноярск'),
+(13, 'Воронеж'),
+(14, 'Пермь'),
+(15, 'Волгоград'),
+(16, 'Краснодар'),
+(17, 'Саратов'),
+(18, 'Тюмень'),
+(19, 'Тольятти'),
+(20, 'Ижевск');
 
 -- --------------------------------------------------------
 
@@ -48,43 +85,6 @@ INSERT INTO `education` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `sities`
---
-
-CREATE TABLE `sities` (
-  `id` int(11) NOT NULL,
-  `name` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `sities`
---
-
-INSERT INTO `sities` (`id`, `name`) VALUES
-(1, 'Москва'),
-(2, 'Санкт-Петербург'),
-(3, 'Новосибирск'),
-(4, 'Екатеринбург'),
-(5, 'Казань'),
-(6, 'Нижний Новгород'),
-(7, 'Челябинск'),
-(8, 'Самара'),
-(9, 'Омск'),
-(10, 'Ростов-на-Дону'),
-(11, 'Уфа'),
-(12, 'Красноярск'),
-(13, 'Воронеж'),
-(14, 'Пермь'),
-(15, 'Волгоград'),
-(16, 'Краснодар'),
-(17, 'Саратов'),
-(18, 'Тюмень'),
-(19, 'Тольятти'),
-(20, 'Ижевск');
-
--- --------------------------------------------------------
-
---
 -- Структура таблицы `users`
 --
 
@@ -100,10 +100,10 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `education_id`, `name`) VALUES
 (1, 1, 'Джефф Безос'),
-(2, 5, 'Билл Гейтс'),
-(3, 2, 'Бернар Арно'),
-(4, 6, 'Уоррен Баффет'),
-(5, 4, 'Ларри Эллисон'),
+(2, 4, 'Билл Гейтс'),
+(3, 1, 'Бернар Арно'),
+(4, 2, 'Уоррен Баффет'),
+(5, 1, 'Ларри Эллисон'),
 (6, 2, 'Амансио Ортега'),
 (7, 1, 'Марк Цукерберг'),
 (8, 2, 'Джим Уолтон');
@@ -111,19 +111,19 @@ INSERT INTO `users` (`id`, `education_id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `user_sity`
+-- Структура таблицы `user_city`
 --
 
-CREATE TABLE `user_sity` (
+CREATE TABLE `user_city` (
   `user_id` int(11) NOT NULL,
-  `sity_id` int(11) NOT NULL
+  `city_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Дамп данных таблицы `user_sity`
+-- Дамп данных таблицы `user_city`
 --
 
-INSERT INTO `user_sity` (`user_id`, `sity_id`) VALUES
+INSERT INTO `user_city` (`user_id`, `city_id`) VALUES
 (1, 3),
 (1, 6),
 (2, 1),
@@ -142,16 +142,16 @@ INSERT INTO `user_sity` (`user_id`, `sity_id`) VALUES
 --
 
 --
+-- Индексы таблицы `cities`
+--
+ALTER TABLE `cities`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Индексы таблицы `education`
 --
 ALTER TABLE `education`
   ADD PRIMARY KEY (`id`) USING BTREE;
-
---
--- Индексы таблицы `sities`
---
-ALTER TABLE `sities`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `users`
@@ -161,26 +161,26 @@ ALTER TABLE `users`
   ADD KEY `education_id` (`education_id`);
 
 --
--- Индексы таблицы `user_sity`
+-- Индексы таблицы `user_city`
 --
-ALTER TABLE `user_sity`
-  ADD KEY `sity_id` (`sity_id`),
-  ADD KEY `user_id` (`user_id`) USING BTREE;
+ALTER TABLE `user_city`
+  ADD KEY `user_id` (`user_id`) USING BTREE,
+  ADD KEY `city_id` (`city_id`) USING BTREE;
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
 --
+-- AUTO_INCREMENT для таблицы `cities`
+--
+ALTER TABLE `cities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+--
 -- AUTO_INCREMENT для таблицы `education`
 --
 ALTER TABLE `education`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
---
--- AUTO_INCREMENT для таблицы `sities`
---
-ALTER TABLE `sities`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
@@ -197,11 +197,11 @@ ALTER TABLE `users`
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`education_id`) REFERENCES `education` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Ограничения внешнего ключа таблицы `user_sity`
+-- Ограничения внешнего ключа таблицы `user_city`
 --
-ALTER TABLE `user_sity`
-  ADD CONSTRAINT `user_sity_ibfk_1` FOREIGN KEY (`sity_id`) REFERENCES `sities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_sity_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `user_city`
+  ADD CONSTRAINT `user_city_ibfk_1` FOREIGN KEY (`city_id`) REFERENCES `cities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_city_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
